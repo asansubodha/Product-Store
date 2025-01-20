e83e6617341761093f7a144d9b320053a261f81b
R.M Asan Subodha Madusankha <ashansubodha8@gmail.com>
2025-01-20 12:59:20 +0530

    00
    ff

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <nav className={`bg-white border-gray-200 dark:bg-gray-900 ${theme === 'dark' ? 'dark' : ''}`}>
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="../pages/Home.jsx" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-8" alt="Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-black dark:text-white">Product Store</span>
         </a>
@@ -26,7 +26,7 @@ const Navbar = () => {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 
           rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a href="../pages/Create.jsx" className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
+              <a href="/create" className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0
                md:p-0 text-black dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
                 <img src={createIcon} alt="create" />
               </a>
--- FrontEnd/src/pages/Create.jsx
+++ FrontEnd/src/pages/Create.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useProductStore } from '../store/product';
 
 const create = () => {
+  const [newProduct, setNewProduct] = useState({
+    name:"",
+    price:"",
+    image:"",
+  });
+
+  const {createProduct} = useProductStore();
+
+  const handleAddProduct = async() =>{
+    const {success, message } = await createProduct(newProduct);
+    console.log("Success", success);
+    console.log("Message", message);
+  };
+
   return (
-    <div>
-      create page
+    <div className='max-w-lg md:mx-auto'>
+      <div className='text-center mb-8 mt-20'>
+        <h1 className='text-4xl font-bold'>Create New Product</h1>
+      </div>
+      
+      <div className='bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md'>
+        <div className='space-y-4'>
+          <input
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
+            placeholder='Product Name'
+            name='name'
+            value={newProduct.name}
+            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} 
+            />
+            <input
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
+            placeholder='Product Price'
+            name='price'
+            value={newProduct.price}
+            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })} 
+            />
+            <input
+            className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
+            placeholder='Image URL'
+            name='image'
+            value={newProduct.image}
+            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })} 
+            />
+            <button
+            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition"
+            onClick={handleAddProduct}
+            >
+            Add Product
+            </button>
+        </div>
+        
+      </div>      
     </div>
   )
 }
